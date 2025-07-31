d6e9c31400c60ee6399ab639bace077f68a75d0c
Portfolio Developer <dev@portfolio.local>
2025-07-31 09:26:54 +0500

    Add password validation

--- src/validation.js
+++ src/validation.js
@@ -3,4 +3,8 @@ function validateEmail(email) {
     return email.includes('@');
 }
 
+function validatePassword(password) {
+    return password.length >= 8;
+}
+
 module.exports = { validateEmail };
