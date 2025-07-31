7d3b67249041def3bfaf76fbb960f8a5a6351037
Portfolio Developer <dev@portfolio.local>
2025-07-31 09:25:01 +0500

    WIP: debugging

--- src/auth.js
+++ src/auth.js
@@ -5,4 +5,7 @@ function login(username, password) {
 function checkCredentals(username, password) {  // опечатка в названии
     return username && password;
 }
+
+console.log('Debug: auth module loaded');  // отладочный код
+
 module.exports = { login };
